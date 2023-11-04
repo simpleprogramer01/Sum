4096f6aebd577467318948521e7960458e338998
Unknown :) <147919329+simpleprogramer01@users.noreply.github.com>
2023-11-04 14:02:19 +0330

    Add files via upload

---
+++ Sum of numbers from 1 to 1000.py
@@ -0,0 +1,9 @@
+#simple programer 
+#Script to calculate numbers from 1 to 1000
+#list :
+ALL = 0
+#conditions :
+for num1 in range(1, 1001):
+    ALL += num1
+#prints : 
+print("The sum of numbers from 1 to 1000 is equal to :", ALL)
